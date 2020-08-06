046d91dad0c1f3db4ab37ae913b36058c6c36f0e
Hunter Danielson <hunterleedanielson@gmail.com>
2020-08-05 17:13:33 -0700

    markdownList completed and added css

--- src/components/markdownList/MarkdownList.jsx
+++ src/components/markdownList/MarkdownList.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import styles from './MarkdownList.css';
 
 const MarkdownList = () => {
-  const markdownList = ['stuff', 'other', 'more', 'even more'];
+  // this is pretend state for now, will change to real state later
+  const markdownList = ['stuff', 'other', 'more', 'even more', 'stuff', 'other', 'more', 'even more', 'stuff', 'other', 'more', 'even more', 'stuff', 'other', 'more', 'even more', 'stuff', 'other', 'more', 'even more', 'stuff', 'other', 'more', 'even more', 'stuff', 'other', 'more', 'even more', 'stuff', 'other', 'more', 'even more'];
   const markdownListElements = markdownList.map((markdown, index) => (<li key={markdown + index}>
     {index}
   </li>));
 
   return (
-    <div>
+    <div className={styles.MarkdownList}>
       <ul>
         {markdownListElements}
       </ul>
