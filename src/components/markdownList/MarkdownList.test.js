9e135f5697b271db7ce5b05c22fc358380e3c6aa
Dan B <dbennington125@gmail.com>
2020-08-06 11:17:52 -0700

    test passes for list state

--- src/components/markdownList/MarkdownList.test.js
+++ src/components/markdownList/MarkdownList.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import MarkdownList from './MarkdownList';
+import AppProvider from '../../provider/AppProvider';
 
 
 describe('MarkdownList component', () => {
   it('renders MarkdownList', () => {
-    const wrapper = shallow(<MarkdownList />);
+    const wrapper = shallow(<AppProvider><MarkdownList /></AppProvider>);
     expect(wrapper).toMatchSnapshot();
   });
 });
